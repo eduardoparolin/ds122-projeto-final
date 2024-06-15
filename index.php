cce8ff3af0ee0842b2c858f730bdb90fd167b83e
eduardoparolin <eduparolins@gmail.com>
2024-06-15 12:06:36 -0300

    initial commit

--- index.php
+++ index.php
@@ -5,29 +5,30 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-
-<div class="titles">
-    <div class="t-holder">
-        <div class="welcome-title">
-            Bem vindo!
+<div class="container">
+    <div class="titles">
+        <div class="t-holder">
+            <div class="welcome-title">
+                Bem vindo!
+            </div>
+            <img class="t-image" src="./assets/tomate.png" alt="tomate"/>
         </div>
-        <img class="t-image" src="./assets/tomate.png" alt="tomate"/>
-    </div>
-    <div class="sub-title">
-        Já conhece a técnica pomodoro de foco?
-    </div>
-    <div>
-        Conheça mais <a href="https://pt.wikipedia.org/wiki/T%C3%A9cnica_pomodoro">aqui</a>
-    </div>
+        <div class="sub-title">
+            Já conhece a técnica pomodoro de foco?
+        </div>
+        <div>
+            Conheça mais <a href="https://pt.wikipedia.org/wiki/T%C3%A9cnica_pomodoro">aqui</a>
+        </div>
 
-    <div class="login-signup-page">
-    <div>
-        É a sua primeira vez aqui? <a href="cadastro.php">clique aqui</a> para realizar cadastro
-    </div>
+        <div class="login-signup-page">
+            <div>
+                É a sua primeira vez aqui? <a href="cadastro.php">clique aqui</a> para realizar cadastro
+            </div>
 
-    <div>
-        OU <a href="login.php">clique aqui</a> para realizar login
-    </div>
+            <div>
+                OU <a href="login.php">clique aqui</a> para realizar login
+            </div>
+        </div>
     </div>
 </div>
 
